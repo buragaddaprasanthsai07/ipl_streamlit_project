b991018ae2dff9d3542e700607225f9b10607528
buragaddaprasanthsai07 <buragaddaprasanthsai07@gmail.com>
2026-09-11 16:43:06 +0530

    Update password for login authentication

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ password = st.text_input(
 type="password"
 )
 if st.button("Login"):
-    if username == "admin" and password == "1234":
+    if username == "admin" and password == "250200203":
         st.session_state.logged_in = True
         st.success("Login successful!")
         st.switch_page("pages/1 _About_Project.py")
